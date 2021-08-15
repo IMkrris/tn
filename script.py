136ba25f525c4b2c63fe7e5cfa15f78b36e4d43e
Im_Krrish <88702957+IMkrris@users.noreply.github.com>
2021-08-15 20:55:56 +0530

    Update script.py

--- script.py
+++ script.py
@@ -16,35 +16,6 @@ Hit <i>/help</i> for more information.</b>
 /help - <code>Command help</code>
 /about -<code>Something about me!</code>
 
-
-<b>Filter Commands;</b>
-
-<code>/add name reply</code>  -  Add filter for name
-
-<code>/del name</code>  -  Delete filter
-
-<code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
-<code>/viewfilters</code>  -  List all filters in chat
-
-
-<b>Connection Commands;</b>
-
-<code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
-<code>/connect</code> in groups.
-
-<code>/connections</code>  -  Manage your connections.
-
-
-<b>Extras;</b>
-
-/status  -  Shows current status of your bot (Auth User Only)
-
-/id  -  Shows ID information
-
-<code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
-
 @TN_Bots
 """
 
